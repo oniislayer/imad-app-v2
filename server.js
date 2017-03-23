bafa713d9fddeaf1d4ffa79fd2efc027f69c8d53
oniislayer <koushik.tamil@gmail.com>
2017-03-23 21:42:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 app.get('/article-one',function(req,res)
 {
-   res.send('here is article 1');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res)
 {
