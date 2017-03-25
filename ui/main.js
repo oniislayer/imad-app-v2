381103f6fb31ff21be0f4d0f49ac8f8e94aaccba
oniislayer <koushik.tamil@gmail.com>
2017-03-25 13:24:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ e.innerHTML="JEssie lets cook";
 
 var i=document.getElementById('mad');
 
+var moveright=function(){
+    marginLeft=marginLeft + 10;
+  i.style.marginLeft=marginLeft+'px';
+};
 i.onclick= function() {
-    i.style.marginLeft='100px';
+
+    var int=setInterval(moveright,100);
 };
