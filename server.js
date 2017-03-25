f58282c5d17fe237907559459444575c99ca91a2
oniislayer <koushik.tamil@gmail.com>
2017-03-25 14:01:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,6 +97,12 @@ res.send(createtemp(arts[artname]));
     
 });
 
+var counter=0;
+app.get('/counter', function(req,res) {
+   counter+=1;
+   res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
