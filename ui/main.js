b44b6dd2820c6018cbd496dfddd696082f76d504
oniislayer <koushik.tamil@gmail.com>
2017-03-25 13:36:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,8 +6,10 @@ var i=document.getElementById('mad');
 var marginLeft=0;
 function moveright()
 {
+  if(marginLeft===0){
   marginLeft = marginLeft + 5;
   i.style.marginLeft = marginLeft + 'px';
+  }
   if(marginLeft===100)
   {
       marginLeft = marginLeft - 5;
