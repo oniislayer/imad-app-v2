26030fd44461e075624644ba7e63f271e17366ae
oniislayer <koushik.tamil@gmail.com>
2017-03-25 17:53:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,9 +31,11 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/name', function(req,res){
-   var namei=req.params.name;
-   names.push(namei);
+app.get('/submitname/name', function(req,res){
+   var name=req.params.name;
+   
+   names.push(name);
+   
    res.send(JSON.stringify(names));
 });
 
