4e1281d7cb388f177831b73c5aa0ed999c7259fc
oniislayer <koushik.tamil@gmail.com>
2017-03-25 13:17:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var e = document.getElementById('txt');
 e.innerHTML="JEssie lets cook";
+
+var i=document.getElementById('mad');
+
+i.onclick= function() {
+    i.style.marginLeft='100px';
+};
