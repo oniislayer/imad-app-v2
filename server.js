46c4894ca8462dcc5fdb0e2e9a76998a4dbba639
oniislayer <koushik.tamil@gmail.com>
2017-03-25 23:36:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config={
+    user: 'oniislayer',
+    database: 'oniislayer',
+    host:'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -21,6 +31,22 @@ app.get('/counter', function(req,res){
     counter=counter + 1;
     res.send(counter.toString());
 });
+var pool = new Pool(config);
+app.get('/testdb', function(req,res){
+   //make req
+   //make response
+   pool.query('SELECT * FROM test'),function(err,res){
+       if(err)
+       {
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           res.send(JSON.stringify(result));
+       }
+   }
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
